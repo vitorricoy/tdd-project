8f1d6ea62ed789f6501f0959f0f113436c00dcfa
Vitor Rodarte <vitorricoy@gmail.com>
2021-03-01 16:32:33 -0300

    Usando a variável de ambiente para ler a senha do email

--- superlists/settings.py
+++ superlists/settings.py
@@ -106,6 +106,11 @@ AUTHENTICATION_BACKENDS = [
     'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'vitorricoy.tdd.project@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
