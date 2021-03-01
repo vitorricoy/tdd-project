7397c7526491b83a38eaa31f84b0c5ab19c7af83
Vitor Rodarte <vitorricoy@gmail.com>
2021-03-01 16:19:48 -0300

    Uando mock.return_value

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -68,4 +68,12 @@ class LoginViewTest(TestCase):
             mock_auth.authenticate.call_args,  
             call(uid='abcd123')  
         )
+    
+    @patch('accounts.views.auth')  
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.login.call_args,  
+            call(response.wsgi_request, mock_auth.authenticate.return_value)  
+        )
 
--- accounts/views.py
+++ accounts/views.py
@@ -25,5 +25,7 @@ def send_login_email(request):
     return redirect('/')
 
 def login(request):
-    auth.authenticate(uid=request.GET.get('token'))
+    user = auth.authenticate(uid=request.GET.get('token'))
+    auth.login(request, user)
     return redirect('/')
+
